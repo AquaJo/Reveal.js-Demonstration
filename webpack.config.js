6a1332e0f6a994a88f69ca9e28d9a867d7c3d74c
AquaJo <84229101+AquaJo@users.noreply.github.com>
2024-07-09 12:11:59 +0200

    url temporary fixed, package update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BundleAnalyzerPlugin =
